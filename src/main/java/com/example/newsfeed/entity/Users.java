f6e9762177decdcb7602e187e9566fc92455b4f4
yeonju <qoduswn9810@gmail.com>
2025-08-07 13:31:54 +0900

    feat/entity

--- src/main/java/com/example/newsfeed/entity/Users.java
+++ src/main/java/com/example/newsfeed/entity/Users.java
@@ -1,4 +1,33 @@
 package com.example.newsfeed.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Where(clause = :"is_deleted = false")
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+    private String password;
+    private String email;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
+
+    public void softDelete() {
+    this.isDeleted = true;
+    }
 }
