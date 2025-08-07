1d23d2b737b74002ca91c28872004955fb054048
yeonju <qoduswn9810@gmail.com>
2025-08-07 21:07:26 +0900

    improve/UsersService

--- src/main/java/com/example/newsfeed/service/UsersService.java
+++ src/main/java/com/example/newsfeed/service/UsersService.java
@@ -9,9 +9,18 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UsersService {
     private final UsersRepository usersRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public void softDeleteUser(String username) {
-        Users users = usersRepository.findByUsernameAndIsDeletedFalse(username).orElseThrow(() -> new UsersNotFoundException("존재하지 않거나 이미 삭제된 사용자입니다."));
+    public void softDeleteUser(String username, String password) {
+        Users users = usersRepository.findByUsername(username).orElseThrow(() -> new UsersNotFoundException("사용자를 찾을 수 없습니다."));
+
+        if (users.isDeleted()) {
+            throw new AlreadyDeletedUsersException("이미 탈퇴된 사용자입니다.");
+        }
+
+        if (!passwordEncoder.matches(password, users.getPassword())) {
+            throw new InvalidPasswordException("비밀번호가 일치하지 않습니다.");
+        }
 
         users.softDelete();
         usersRepository.save(users);
