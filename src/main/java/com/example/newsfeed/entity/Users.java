98a4a8d06ff5e089ad636fb8524d738d4acabebe
YongHyen-2614 <jkljkl0614@gmail.com>
2025-08-08 15:44:33 +0900

    Merge branch 'dev' into feature/cyh
    # Conflicts:
    #	src/main/java/com/example/newsfeed/dto/temporary
    ✨ feat: 프로필 조회, 수정 및 비밀번호 변경 추가

--- src/main/java/com/example/newsfeed/entity/Users.java
+++ src/main/java/com/example/newsfeed/entity/Users.java
@@ -1,4 +1,46 @@
 package com.example.newsfeed.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Users {
+
+    @Column(nullable = false)
+    private boolean deleted = false;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String name;
+    private String nickname;
+    private String bio;
+    private String profileImage;
+    private String hometown;
+    private String school;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
