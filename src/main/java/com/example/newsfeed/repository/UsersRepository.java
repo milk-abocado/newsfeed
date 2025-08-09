a3bebd1f285ec2a77ba9aad1f29341d269ba0269
yeonju <qoduswn9810@gmail.com>
2025-08-09 15:08:54 +0900

    fix/UsersRepository

--- src/main/java/com/example/newsfeed/repository/UsersRepository.java
+++ src/main/java/com/example/newsfeed/repository/UsersRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
-    Optional<Users> findByName(String name);
-    Optional<Users> findByNameAndIsDeleted(String name, Users.getIsDeleted isDeleted);
+    Optional<Users> findByName(String email);
+    Optional<Users> findByNameAndIsDeletedFalse(String email);
 }
