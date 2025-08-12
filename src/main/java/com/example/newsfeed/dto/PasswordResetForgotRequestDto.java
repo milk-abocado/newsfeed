ee7da30a89a9c138bcdc934aa362989ade789cd5
dori135 <nolli77@ewha.ac.kr>
2025-08-12 20:13:31 +0900

    ⚡️ improve: 비밀번호 찾기용 질문 답 받고 일치하면 넘어가도록 수정 + 비밀번호 형식 검사 추가

--- src/main/java/com/example/newsfeed/dto/PasswordResetForgotRequestDto.java
+++ src/main/java/com/example/newsfeed/dto/PasswordResetForgotRequestDto.java
@@ -11,4 +11,6 @@ public class PasswordResetForgotRequestDto {
     @NotBlank(message = "이메일을 입력해주세요.")
     @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
+    private String securityQuestion;
+    private String securityAnswer;
 }
